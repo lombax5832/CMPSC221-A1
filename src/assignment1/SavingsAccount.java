69e6f1b3af1c917e06f4c74ddbf349e75535729b
aps5601 <aps5601@PatteeW116-44.win.psu.edu>
2017-08-31 14:46:17 -0400

    Place main within SavingsAccount. Avoid overridable methods in constructors. Use %n instead of \n in printf() for greater compatibility.

--- src/assignment1/SavingsAccount.java
+++ src/assignment1/SavingsAccount.java
@@ -18,8 +18,14 @@ public class SavingsAccount {
     private static BigDecimal annualInterestRate;
     private BigDecimal savingsBalance;
 
+    SavingsAccount(BigDecimal balance){
+        //Avoiding overridable methods in constructors
+        savingsBalance = balance;
+    }
+        
     SavingsAccount(double balance) {
-        setSavingsBalance(balance);
+        //Avoiding overridable methods in constructors
+        savingsBalance = BigDecimal.valueOf(balance);
     }
 
     SavingsAccount() {
@@ -103,5 +109,49 @@ public class SavingsAccount {
     
     public static String getAnnualInterestRateString() {
         return NumberFormat.getNumberInstance().format(annualInterestRate);
+    }
+    
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        
+        //Initialize 2 new SavingsAccount with initial values of 2000 and 3000
+        SavingsAccount account1 = new SavingsAccount(2000.00);
+        SavingsAccount account2 = new SavingsAccount(3000.00);
+        
+        //Initial interest rate is 4%
+        SavingsAccount.setAnnualInterestRate(0.04);
+        
+        //Format table header
+        System.out.printf("%-7s%15s%15s%n",
+                "Month", 
+                "Account1", 
+                "Account2");
+        
+        int i;
+        
+        //Calculate Interest rate for 12 months
+        for(i = 1; i < 13; i++){
+            account1.calculateMonthlyInterest();
+            account2.calculateMonthlyInterest();
+            
+            System.out.printf("%-7d%15s%15s%n", i, 
+                    account1.getBalanceString(), 
+                    account2.getBalanceString());
+            
+        }
+        
+        //Set new interest rate to 5%
+        SavingsAccount.setAnnualInterestRate(0.05);
+        
+        //Interest Rate changes for 1 last month
+        account1.calculateMonthlyInterest();
+        account2.calculateMonthlyInterest();
+        
+        System.out.printf("%-7d%15s%15s\n", i, 
+                    account1.getBalanceString(), 
+                    account2.getBalanceString());
+        
     }
 }
